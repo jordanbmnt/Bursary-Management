fa7a3c6b800e400693ba1201f10f4dbbe7b10c74
Jordan Beaumont <Jordan.Beaumont@bbd.co.za>
2024-02-15 19:09:53 +0200

    added RejectedStudentsView

---
+++ src/db/dbManipulation/Views/RejectedStudentsiew.sql
@@ -0,0 +1,19 @@
+USE BBD_BursaryDB;
+
+GO
+
+CREATE VIEW RejectedStudents
+AS
+  SELECT S.[StudentID], U.[FirstName], U.[LastName], B.[BursaryApplicantStatus]
+  FROM (
+    [Student] AS S
+    INNER JOIN [User_Details] AS U
+    ON S.[UserID] = U.[UserID]
+    )
+    INNER JOIN (
+    [Bursary_Applicants] AS B
+    INNER JOIN [Application_Status] AS A
+    ON B.[BursaryApplicantStatus] = A.[Index]
+    )
+    ON B.[StudentID] = A.[Index]
+  WHERE A.[Status] = ('rejected');
