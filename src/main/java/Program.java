4e85206fa509c5a1842ca0caed27f833d00f573b
Jordan Beaumont <Jordan.Beaumont@bbd.co.za>
2024-02-16 08:45:02 +0200

    added folder structure

---
+++ src/main/java/Program.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program {
+        public static void main(String[] args) {
+                SpringApplication.run(Program.class, args);
+        }
+
+
+}
