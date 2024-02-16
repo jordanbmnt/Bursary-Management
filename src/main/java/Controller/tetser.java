bc4d3d970a3f96910fd51da1c389a91049264ff9
Jordan Beaumont <Jordan.Beaumont@bbd.co.za>
2024-02-16 14:18:24 +0200

    added procedure for AddBursaryApplicantTemplate

---
+++ src/main/java/Controller/tetser.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class tetser {
+    @RequestMapping("/")
+    String home() {
+        return "Our API is working!";
+    }
+}
