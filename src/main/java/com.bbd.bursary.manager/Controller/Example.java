f6a80cc1dd2cba3aea2e19a50faad7f7e17764c4
Jordan Beaumont <jordanbmnt@gmail.com>
2024-02-17 11:34:49 +0200

    Updated pom and added example controller for testing

---
+++ src/main/java/com.bbd.bursary.manager/Controller/Example.java
@@ -0,0 +1,16 @@
+package com.bbd.bursary.manager.Controller;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Our API is working!";
+    }
+
+}
--- src/main/java/com.bbd.bursary.manager/Program.java
+++ src/main/java/com.bbd.bursary.manager/Program.java
@@ -1,6 +1,11 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+package com.bbd.bursary.manager;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Program {
         public static void main(String[] args) {
