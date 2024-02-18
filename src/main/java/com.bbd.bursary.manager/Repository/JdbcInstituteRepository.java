fd04770a29eb3d7bc9e01e50858128155097f3a0
mraees93 <raeesdolley76@gmail.com>
2024-02-18 12:35:33 +0200

    added <artifactId>spring-jdbc</artifactId>

--- src/main/java/com.bbd.bursary.manager/Repository/JdbcInstituteRepository.java
+++ src/main/java/com.bbd.bursary.manager/Repository/JdbcInstituteRepository.java
@@ -1,5 +1,14 @@
 package com.bbd.bursary.manager.Repository;
 
-public class JdbcInstituteRepository {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class JdbcInstituteRepository implements InstituteInterface {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public int save(Institute institute) {
+        return jdbcTemplate.update("INSERT INTO Institute_Info")
+    }
 }
