de16dd7d47bac4f81cf10a9441743bdddbf75989
Jordan Beaumont <jordanbmnt@gmail.com>
2024-02-18 12:23:04 +0200

    dummy push

--- src/db/dbManipulation/Procedures/deleteBBDUserById.sql
+++ src/db/dbManipulation/Procedures/deleteBBDUserById.sql
@@ -1,2 +1,25 @@
 CREATE PROCEDURE DeleteBBDUserById
   @UserID INT
+AS
+BEGIN
+
+  IF EXISTS (SELECT 1
+  FROM User_Details
+  WHERE UserID = @UserID)
+    BEGIN
+    DELETE FROM Bursary_Applicants WHERE StudentID = @StudentId;
+  END
+
+  IF EXISTS (SELECT 1
+  FROM Bursary_Applicant_Grades
+  WHERE BursaryApplicantID IN (SELECT BursaryApplicantID
+  FROM Bursary_Applicants
+  WHERE StudentID = @StudentId))
+    BEGIN
+    DELETE FROM Bursary_Applicant_Grades WHERE BursaryApplicantID IN (SELECT BursaryApplicantID
+    FROM Bursary_Applicants
+    WHERE StudentID = @StudentId);
+  END
+
+  DELETE FROM Student WHERE StudentID = @StudentId;
+END;
