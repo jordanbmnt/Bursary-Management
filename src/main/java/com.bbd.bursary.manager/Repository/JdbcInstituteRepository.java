aa48ca530194904b49400aee604ab3d4e27093ca
mraees93 <raeesdolley76@gmail.com>
2024-02-18 13:55:08 +0200

    added institute repository code

--- src/main/java/com.bbd.bursary.manager/Repository/JdbcInstituteRepository.java
+++ src/main/java/com.bbd.bursary.manager/Repository/JdbcInstituteRepository.java
@@ -1,6 +1,8 @@
 package com.bbd.bursary.manager.Repository;
 
+import com.bbd.bursary.manager.Model.Institute;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,6 +11,21 @@ public class JdbcInstituteRepository implements InstituteInterface {
     private JdbcTemplate jdbcTemplate;
 
     public int save(Institute institute) {
-        return jdbcTemplate.update("INSERT INTO Institute_Info")
+        return jdbcTemplate.update("INSERT INTO Institute_Info (InstituteName, ContactDetailsID, BBDBursaryInstituteStatus) VALUES (?,?,?)",
+                new Object[] {
+                        institute.getInstituteName()
+                });
+    }
+
+//    public int update(Institute institute) {
+//        return jdbcTemplate.update("UPDATE Institution_fund_allocation SET AllocatedAmount WHERE InstituteId=?",
+//                new Object[] {institute.}
+//                )
+//    }
+
+    public int update(Institute institute) {
+        return jdbcTemplate.update("UPDATE Institute_info SET BBDBursaryInstituteStatus WHERE InstituteID = ?",
+                new Object[] { institute.setBbdBursaryInstituteStatus()}
+                )
     }
 }
