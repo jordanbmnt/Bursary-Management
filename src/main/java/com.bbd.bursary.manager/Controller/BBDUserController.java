e427b7cf3e79003c5c6742226d8736bbadb596ad
AndileCartist <andilemkhizekhabazela@gmail.com>
2024-02-19 11:44:47 +0200

    modified user controller

--- src/main/java/com.bbd.bursary.manager/Controller/BBDUserController.java
+++ src/main/java/com.bbd.bursary.manager/Controller/BBDUserController.java
@@ -2,27 +2,24 @@ package com.bbd.bursary.manager.Controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import com.bbd.bursary.manager.Model.User;
+import  com.bbd.bursary.manager.Repository.BBDInterface;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bbd.bursary.manager.Model.User;
-import com.bbd.bursary.manager.Repository.BBDInterface;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
 
 
 public class BBDUserController {
@@ -37,16 +34,16 @@ public class BBDUserController {
 
             bbdInterface.findAllInstitutions().forEach(users::add);
             if (users.isEmpty()) {
+                System.out.println("Entered");
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
-            System.out.println("success");
             return new ResponseEntity<>(users, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @PutMapping("/institutions/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateTutorial(@PathVariable("id") long id, @RequestBody User user) {
         User _user = bbdInterface.findById(id);
 
--- src/main/java/com.bbd.bursary.manager/Repository/JbdcUserRepository.java
+++ src/main/java/com.bbd.bursary.manager/Repository/JbdcUserRepository.java
@@ -1,38 +1,40 @@
 package com.bbd.bursary.manager.Repository;
 
-
-import com.bbd.bursary.manager.Model.User;
+//import com.bbd.bursary.manager.Model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.stereotype.Repository;
+import com.bbd.bursary.manager.Model.User;
 
 import java.util.List;
 @Repository
-public class jbdcUserRepository implements BBDInterface {
+public class JbdcUserRepository implements BBDInterface {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Override
     public int save(User user) {
+       final String sql = "EXEC [BBD_BursaryDB].[dbo].[addBBDUserProcedure] @FirstName = ?, @LastName = ?, @PhoneNumber = ?, @Email = ?;";
         return jdbcTemplate.update(
-                "EXEC [BBD_BursaryDB].[dbo].[addBBDUserProcedure] @FirstName = ?, @LastName = ?, @PhoneNumber = ?, @Email = ?;",
+                sql,
                 new Object[]{user.getFirstName(), user.getLastName(), user.getPhoneNumber(), user.getEmail()});
     }
 
     @Override
     public int update(User user) {
-        return jdbcTemplate.update("EXEC [BBD_BursaryDB].[dbo].[UpdateBBDUserInfo] @UserId =  ?, @FirstName = ?, @LastName = ?, @Email = ?, @PhoneNumber = ?",
+       final String sql = "EXEC [BBD_BursaryDB].[dbo].[UpdateBBDUserInfo] @UserId =  ?, @FirstName = ?, @LastName = ?, @Email = ?, @PhoneNumber = ?";
+        return jdbcTemplate.update(sql,
                 new Object[]{user.getUserId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getPhoneNumber()});
     }
 
     @Override
     public User findById(Long id) {
         try {
-            User user = jdbcTemplate.queryForObject("SELECT * FROM User_Details WHERE id=?",
+           final String sql = "SELECT * FROM User_Details WHERE id=?";
+            User user = jdbcTemplate.queryForObject(sql,
                     BeanPropertyRowMapper.newInstance(User.class), id);
-
             return user;
         } catch (IncorrectResultSizeDataAccessException e) {
             return null;
@@ -40,19 +42,22 @@ public class jbdcUserRepository implements BBDInterface {
     }
     @Override
     public int deleteUserById(Long id) {
-        return jdbcTemplate.update("EXEC [BBD_BursaryDB].[dbo].[DeleteBBDUserById] @UserId=?", id);
+       final String sql = "EXEC [BBD_BursaryDB].[dbo].[DeleteBBDUserById] @UserId=?";
+        return jdbcTemplate.update(sql, id);
     }
 
     @Override
     public int allocateFunds(long id, int amount) {
+       final String sql = "UPDATE [BBD_BursaryDB].[dbo].[Institution_Fund_Allocation] SET AllocatedAmount=? WHERE UserID=?";
         return jdbcTemplate.update(
-                "UPDATE [BBD_BursaryDB].[dbo].[Institution_Fund_Allocation] SET AllocatedAmount=? WHERE UserID=?",
+                sql,
                 amount, id);
     }
 
 
     @Override
     public List<User> findAllInstitutions() {
-        return jdbcTemplate.query("SELECT * from Institute_Info", BeanPropertyRowMapper.newInstance(User.class));
+       final String sql = "SELECT * from Institute_Info";
+        return jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(User.class));
     }
 }
