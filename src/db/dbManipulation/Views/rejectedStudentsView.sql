30b88a0f6cbb847693975012232059260dd241ae
Jordan Beaumont <jordanbmnt@gmail.com>
2024-02-19 10:46:57 +0200

    updated views

--- src/db/dbManipulation/Views/rejectedStudentsView.sql
+++ src/db/dbManipulation/Views/rejectedStudentsView.sql
@@ -2,12 +2,14 @@ USE BBD_BursaryDB;
 
 GO
 
-CREATE VIEW RejectedStudents
+CREATE VIEW RejectedStudent
 AS
-  SELECT S.[StudentID], U.[FirstName], U.[LastName], B.[BursaryApplicantStatus]
+  SELECT S.[StudentID], U.[FirstName], U.[LastName], B.[BursaryApplicantStatus], C.[Email], C.[PhoneNumber]
   FROM [Student] AS S
     INNER JOIN [User_Details] AS U
     ON S.[UserID] = U.[UserID]
+    INNER JOIN [Contact_Details] AS C
+     ON U.[ContactDetailsID] = C.[ContactDetailsID]
     INNER JOIN [Bursary_Applicants] AS B
     ON S.[StudentID] = B.[StudentID]
     INNER JOIN [Application_Status] AS A
