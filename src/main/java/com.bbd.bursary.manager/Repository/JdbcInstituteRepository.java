b0a6ee86089443c1390ad6d4374679c6b904d530
AndileCartist <andilemkhizekhabazela@gmail.com>
2024-02-20 09:54:24 +0200

    added views on yml

--- src/main/java/com.bbd.bursary.manager/Repository/JdbcInstituteRepository.java
+++ src/main/java/com.bbd.bursary.manager/Repository/JdbcInstituteRepository.java
@@ -27,7 +27,6 @@ public class JdbcInstituteRepository implements InstituteInterface {
                         institute.getPhoneNumber(),
                         institute.getEmail(),
                         institute.getBbdBursaryInstituteStatus(),
-//                      institute.getAllocatedAmount()
                 });
     }
 
